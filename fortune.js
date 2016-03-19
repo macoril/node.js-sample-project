47c1b326647ca5d396173403680afe8f595a2338
macoril <macoriloid@gmail.com>
2016-03-19 09:23:34 +0900

    おみくじ途中まで(Syntax Errorは解消したが処理書き途中で404状態)
    ↓をアレンジしながら写経
    https://osdn.jp/users/hylom/pf/node_sample_fortune/scm/blobs/d5e2c7fe120c990fa623671c3ef65bc7dbd7ac36/fortune.js
    
    バックスラッシュを使って複数行の文字列を書く方法は、
    Syntax errorが出た＆非推奨のようなので以下の様なやり方にした。
    var str_with_line_breaks = (function() {/*
      blah blah blah...
    */}).toString().match{/\/\*([^]*)\*\//)[1];
    cf. http://qiita.com/koshihikari/items/f6a90d8a19bda831d879

---
+++ fortune.js
@@ -0,0 +1,104 @@
+var PORT = 8080;
+var HOSTNAME = '127.0.0.1';
+
+// load necessary modules
+var http = require('http');
+var querystring = require('querystring');
+var crypto = require('crypto')
+
+//var htmlHeader = '<!DOCTYPE HTML>
+var htmlHeader = (function() {/*
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>fortune telling</title>
+    <style>
+      .content {
+        width: 480px;
+        text-align: center;
+        border; 4px solid lightblue;
+        padding; 4px;
+        margin; 16px auto;
+      }
+      .main-form div {margin-bottom; 4px;}
+      .result {
+        display; black;
+        font-size; 200%;
+        color; red;
+        margin; 4px auto;
+        border; 1px solid;
+        width; 4em;
+      }
+    </style>
+  </html>
+  <body>
+    <div class="content">
+    <h1>fortune telling</h1>
+*/}).toString().match(/\/\*([^]*)\*\//)[1];
+var htmlMainForm = (function() {/*
+<div class="main-form">
+  <form method="post" action="/">
+    <div>
+      <label>名前：<input type="text" name="name" size="20"></label>
+    </div>
+    <div>
+      生年月日：
+      <label><input type="text" name="year" size="5">年</label>
+      <label><input type="text" name="month" size="3">月</label>
+      <label><input type="text" name="day" size="3">日</label>
+    </div>
+    <div>
+      性別：
+      <label><input type="radio" name="sex" value="male">男</label>
+      <label><input type="radio" name="sex" value="female">女</label>
+    </div>
+    <input type="submit" value="占う">
+  </form>
+</div>
+*/}).toString().match(/\/\*([^]*)\*\//)[1];
+
+var htmlFooter = '</div></body></html>';
+
+//TODO 恐らくサニタイズ用のモジュールがあるので、それに置き換える
+// 「<」や「>」、「&」といった文字列をエンティティに変換する
+function escapeHtmlSpecialChar(html) {
+  if (html === undefined) {
+    return ;
+  } else {
+    html = html.replace(/&/g, '&amp;');
+    html = html.replace(/</g, '&lt;');
+    html = html.replace(/>/g, '&gt;');
+    return html;
+  }
+};
+
+http.createServer(function (req, res) {
+  if (req.url !== '/') {
+    res.writeHead(404, {'Content-type': 'text/plain'});
+    res.end('Error 404: Not Found.');
+    return ;
+  }
+
+  // if the request is POST, tell the fortune from data sent.
+  if (req.method === 'POST') {
+    // get data sent
+    req.data = '';
+    req.on('data', function (chunk) {
+      req.data += chunk;
+    });
+    req.on('end', sendResponse);
+    return ;
+
+  // if the request is not POST, send mainform.
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    res.write(htmlHeader);
+    res.write(htmlMainForm);
+    res.write(htmlFooter);
+    res.end();
+    return ;
+  }
+}).listen(PORT);
+
+console.log('Server running at http://' + HOSTNAME + ':' + PORT + '/');

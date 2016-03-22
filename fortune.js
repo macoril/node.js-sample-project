981192e921aef6ff54d718f8b83c146833c89f9a
macoril <macoriloid@gmail.com>
2016-03-23 04:34:23 +0900

    replaceを使った自前のサニタイジングをvalidatorモジュールを用いたサニタイジングへ
    cf.
    validator https://www.npmjs.com/package/validator

--- fortune.js
+++ fortune.js
@@ -66,10 +66,7 @@ function escapeHtmlSpecialChar(html) {
   if (html === undefined) {
     return ;
   } else {
-    html = html.replace(/&/g, '&amp;');
-    html = html.replace(/</g, '&lt;');
-    html = html.replace(/>/g, '&gt;');
-    return html;
+    return require('validator').escape(html);
   }
 };
 

300477e0c09ff28ca42699d90600bca8cc72931b
macoril <macoriloid@gmail.com>
2016-03-25 00:46:13 +0900

    socket.ioのエラーを解消。twitter-nodeのエラーの解消方法がわからず...
    エラー内容:
    $ node web.js
    /Users/macoril/Develop/node.js_sample_project/twithash/web.js:24
      .addListerner('tweet', function(tweet) {
       ^
    
    TypeError: twitter.addListerner is not a function
        at Object.<anonymous> (/Users/macoril/Develop/node.js_sample_project/twithash/web.js:24:4)
        at Module._compile (module.js:413:34)
        at Object.Module._extensions..js (module.js:422:10)
        at Module.load (module.js:357:32)
        at Function.Module._load (module.js:314:12)
        at Function.Module.runMain (module.js:447:10)
        at startup (node.js:139:18)
        at node.js:999:3

--- twithash/web.js
+++ twithash/web.js
@@ -7,13 +7,11 @@ const sys = require('util'),
       TwitterNode = require('twitter-node').TwitterNode;
 
 // express
-//app.configure(function() {
-  app.use(express.static(__dirname + '/views'));
-//});
+app.use(express.static(__dirname + '/views'));
 server.listen(8888);
 
 //socket.io
-const socket = io.listen(app);
+const socket = io.listen(server);
 
 // twitter
 var twitter = new TwitterNode({

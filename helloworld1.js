756b43b440fe8b7c314ae0575f7965d4b4498ff1
macoril <macoriloid@gmail.com>
2016-03-25 11:32:06 +0900

    自前の外部ファイルの読み込み(exportsなしだとアクセス出来ない)

--- helloworld1.js
+++ helloworld1.js
@@ -6,6 +6,11 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>hi!\n\n\n\n\n\nEveryone!!</h1>'); //タグが有効な代わりに\nは効かない
   res.write('<h2>Hello World</h2>');
+
+  var helloName = require('./modules/hello_name');
+  helloName.setName('Taro');
+  helloName.hello();
+
   res.end();
 }).listen(PORT);
 
---
+++ modules/hello_name.js
@@ -0,0 +1,6 @@
+setName = function(name) {
+  this.name = name;
+}
+hello = function() {
+  console.log('Hello,', this.name);
+}

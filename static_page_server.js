f4ed60062365616bb66b7edda04bcca349e51e7b
macoril <macoriloid@gmail.com>
2016-04-01 05:13:26 +0900

    ベースを記述。まだエラーが出る
    出てるエラー:
    Failed to load resource: net::ERR_INCOMPLETE_CHUNKED_ENCODING
    
    参考:
    http://shimz.me/blog/node-js/2690

---
+++ static_page_server.js
@@ -0,0 +1,62 @@
+var PORT = 8888;
+var HOSTNAME = '127.0.0.1';
+
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+
+http.createServer(function (req, res) {
+  var Response = {
+    "200": function(file, filename) {
+      var extname = path.extname(filename);
+      var header = {
+        "Pragma": "no-cache"
+      , "Cache-Control": "no-cache"
+      }
+      res.writeHead(200, header);
+      res.write(file, "binary");
+      res.end;
+    }
+    , "404": function() {
+      var header = {
+        "Content-Type": "text/plain"
+      }
+      res.writeHead(404, header);
+      res.write("404 Not Found\n");
+      res.end;
+    }
+    , "500": function(err) {
+      var header = {
+        "Content-Type": "text/plain"
+      }
+      res.writeHead(500, header);
+      res.write(err + "\n");
+      res.end;
+    }
+  }
+
+  var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), uri);
+
+  fs.exists(filename, function(exists) {
+    console.log(filename + " " + exists);
+    if (!exists) {
+      Response["404"]();
+      return;
+    }
+    if (fs.statSync(filename).isDirectory()) {
+      filename += '/index.html';
+    }
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if (err) {
+        Response["500"](err);
+        return;
+      }
+      Response["200"](file, filename);
+    });
+  });
+}).listen(PORT);
+
+console.log('Server running at http://' + HOSTNAME + ':' + PORT + '/');

5c8b97684699d2aa82382c69cc7e7bb98b19ccb0
macoril <macoriloid@gmail.com>
2016-04-05 17:47:08 +0900

    presetを追加＆変換チェックwの内容がわかりやすいようにリネーム
    presetのインストールとpackage.jsonまたは.babelrcでの指定をしていないと
    適切な変換は行われない。
    
    arrow〜だけ、変換テストだけじゃなくて、実行もできるように少し手を入れた。
    nodeでは変換前の状態でも実行できた

--- src/arrows_and_lexical_this.js
+++ src/arrows_and_lexical_this.js
@@ -1,8 +1,10 @@
 // Expression bodies
+const evens = [2,4,6,8,10];
 var odds = evens.map(v => v + 1);
 var nums = evens.map((v, i) => v + i);
 
 // Statement bodies
+var fives = [1,2,3,4,5];
 nums.forEach(v => {
   if (v % 5 === 0)
     fives.push(v);
@@ -17,3 +19,9 @@ var bob = {
       console.log(this._name + " knows " + f));
   }
 };
+
+console.log(evens);
+console.log(odds);
+console.log(nums);
+console.log(fives);
+console.log(bob);

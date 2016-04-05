7ecacd4353e6eab4994cd1fcdc69bcc1d6344cf3
macoril <macoriloid@gmail.com>
2016-04-05 11:45:38 +0900

    promiseを使って1から順に並ぶようになった
    before:1 3 4 6 2 5
    after: 1 2 3 4 5 6

---
+++ with_promise.js
@@ -0,0 +1,31 @@
+var fs = require("fs");
+
+var f1 = function () {
+  console.log('4');
+  new Promise(function(resolve, reject) {
+    fs.readdir('.', function(err, files) {
+      console.log('5');
+      resolve();
+    });
+  }).then (function() {
+    console.log('6');
+  });
+  return;
+}
+
+new Promise(function(resolve, reject) {
+  fs.stat('helloworld1.js', function(err, stats) {
+    console.log('1');
+    resolve();
+  });
+}).then (function() {
+  return new Promise(function(resolve, reject) {
+    fs.stat('helloworld2.js', function(err, stats) {
+      console.log('2');
+      resolve();
+    });
+  });
+}).then (function() {
+  console.log('3');
+  f1 ();
+});
